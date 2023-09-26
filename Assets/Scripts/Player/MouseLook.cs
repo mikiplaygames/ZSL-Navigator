757cce0af823eee11e4bdd05889f393f85aa09aa
amonguy-irl <145436422+amonguy-irl@users.noreply.github.com>
2023-09-26 10:50:16 +0200

    Camera Look

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 50f;
+    [SerializeField] private Transform playerBody;
+
+    private Vector2 mouseLook;
+    private InputMaster inputMaster;
+    private float rotationX = 0f;
+
+    void Awake()
+    {
+        inputMaster = new InputMaster();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        Look();
+    }
+    private void Look()
+    {
+        mouseLook = inputMaster.Player.Look.ReadValue<Vector2>();
+
+        float mouseX = mouseLook.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = mouseLook.y * mouseSensitivity * Time.deltaTime;
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    private void OnEnable()
+    {
+        inputMaster.Enable();
+    }
+    private void OnDisable()
+    {
+        inputMaster.Disable();
+    }
+}
