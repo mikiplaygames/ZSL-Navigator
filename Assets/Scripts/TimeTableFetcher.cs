f4931293dff982a56fb135f2a009127f8f4d82b2
mikiplaygames <gajewskimikolaj00@gmail.com>
2024-04-14 16:07:26 +0200

    Async voids

--- Assets/Scripts/TimeTableFetcher.cs
+++ Assets/Scripts/TimeTableFetcher.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using HtmlAgilityPack;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TimeTableFetcher : MonoBehaviour
 {
@@ -13,8 +14,7 @@ public class TimeTableFetcher : MonoBehaviour
     private TimeTableCreator tableCreator;
     private List<List<string>> table = new();
     [SerializeField] private TMPro.TMP_Dropdown classesDropdown;
-    [SerializeField] private Button classesButton;
-    [SerializeField] private TMPro.TextMeshProUGUI classText;
+    [SerializeField] private TMPro.TMP_Dropdown lessonDropdown;
     
     private void Awake()
     {
@@ -24,15 +24,11 @@ public class TimeTableFetcher : MonoBehaviour
             Destroy(gameObject);
         tableCreator = GetComponent<TimeTableCreator>();
         htmlDoc = new HtmlDocument();
-        RetriveClasses();
-        classesButton.onClick.AddListener(() =>
-        {
-            //cla
-        });
+        //RetriveClasses();
     }
-    private void RetriveClasses()
+    private async void RetriveClasses()
     {
-        var html = RetriveHtml($"https://www.zsl.gda.pl/assets/plan-lekcji/lista.html");
+        var html = await RetriveHtml($"https://www.zsl.gda.pl/assets/plan-lekcji/lista.html");
         htmlDoc.LoadHtml(html);
         var classList = htmlDoc.DocumentNode.Descendants("a");
         foreach (var VARIABLE in classList)
@@ -44,9 +40,9 @@ public class TimeTableFetcher : MonoBehaviour
             }catch{}
         }
     }
-    public void Reload(int classId)
+    public async void Reload(int classId)
     {
-        var classSchedule = RetriveHtml($"https://www.zsl.gda.pl/assets/plan-lekcji/plany/o{classId}.html");
+        var classSchedule = await RetriveHtml($"https://www.zsl.gda.pl/assets/plan-lekcji/plany/o{classId}.html");
         htmlDoc.LoadHtml(classSchedule);
         var stringTable = htmlDoc.DocumentNode.Descendants("table").First(x => x.GetAttributeValue("class", "") == "tabela");
         table.Clear();
@@ -54,7 +50,6 @@ public class TimeTableFetcher : MonoBehaviour
         {
             table.Add(new List<string>{columnName.InnerText});
         }
-        
         foreach (var row in stringTable.Descendants("tr"))
         {
             int i = 0;
@@ -64,7 +59,6 @@ public class TimeTableFetcher : MonoBehaviour
                 i++;
             }
         }
-
         for (int i = 0; i < table.Count; i++)
         {
             for (int j = 0; j < table[i].Count; j++)
@@ -87,9 +81,11 @@ public class TimeTableFetcher : MonoBehaviour
         var option = new TMPro.TMP_Dropdown.OptionData(name);
         classesDropdown.options.Add(option);
     }
-    private string RetriveHtml(string url)
+    private async Task<string> RetriveHtml(string url)
     {
-        var htmlData = new WebClient().DownloadData(url);
+        Uri uri = new(url);
+        var client = new WebClient();
+        var htmlData = await client.DownloadDataTaskAsync(uri);
         return Encoding.UTF8.GetString(htmlData);
     }
 }
