e848f863bfb290a1fd9690c7c43a378c15173aa8
Doc <82221725+ThatDoc@users.noreply.github.com>
2024-04-15 16:32:56 +0200

    class asset

--- Assets/Scripts/RoomNumber.cs
+++ Assets/Scripts/RoomNumber.cs
@@ -1,12 +1,17 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class RoomNumber : MonoBehaviour// , IInteractable
 {
     [SerializeField] private string id;
+    [SerializeField] TextMeshPro textMeshPro;
     private void Awake()
     {
-        id ??= name;
+        if (string.IsNullOrEmpty(id))
+            id = name;
+        textMeshPro.SetText(id);
+        Debug.Log(id);
     }
     private void Start()
     {
